14eda4bdd31631a847a88359b18eb5329de844f9
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-09 10:59:37 +0800

    react-counter

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-import { useState } from 'react';
+import Counter from './0109/1-counter/counter';
+// import JSXValue from './0109/2.jsx/JSXValue';
 
 function App() {
-  //宣告狀態用語法
-  const [total, setTotal] = useState(0); //0->初始值; total->得到狀態用; setTotal->設定狀態用
   return (
-    //return 相當於 render //react不使用id
     <>
-      <h1
-        onClick={() => {
-          //setTotal(total++) -> 不可使用total ++，因相當於total+=1
-          setTotal(total + 1);
-        }}
-      >
-        {total}
-      </h1>
+      <Counter />
     </>
   );
 }
