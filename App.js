9dccc490f9dd0ac2e1c0ba70cb85ccc35ebebf38
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-09 14:22:35 +0800

    map範例2

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // import Counter from './0109/1-counter/counter';
 // import JSXValue2 from './0109/3.jsx/JSXValue2';
-import Student from './0109/4.map/Student';
+// import Student from './0109/4.map/Student';
+import Student from './0109/4-2.map/Student';
 
 function App() {
   return (
