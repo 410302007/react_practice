891ab6492dc724f2db3dd5b3b9f238f445e85000
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-09 11:39:40 +0800

    css

---
+++ 0109/2.jsx/ex-2/App.js
@@ -0,0 +1,12 @@
+// import Counter from './0109/1-counter/counter';
+import JSXValue from './0109/2.jsx/JSXValue';
+
+function App() {
+  return (
+    <>
+      <JSXValue />
+    </>
+  );
+}
+
+export default App;
