d78366cd4f820bf1b7444149f64c7887a5bccf95
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-10 10:06:16 +0800

    父母子女元件關係- props解構

--- 0110/2.props-1/Child.js
+++ 0110/2.props-1/Child.js
@@ -1,10 +1,13 @@
 function Child(props) {
+  //先解構
+  const { text, color, foo } = props;
   // 子女元件可透過props接收到由父母元件傳遞來的各種資料
-  console.log(props); //text: "Hello你好"
+  // console.log(props); //text: "Hello你好"
   return (
     <>
-      {props.text}
-      <button onClick={props.foo}>按我</button>
+      <div>文字:{text}</div>
+      <div>顏色:{color}</div>
+      <button onClick={foo}>按我</button>
     </>
   );
 }
