47cfb6c5ad73ff602430a27660ad84a0f0324c8b
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-10 17:27:27 +0800

    shoppingCart

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import Exam2 from './0110/5.practice/Exam2';
+import ShoppingCart from './0110/6-1.practice-cart/ShoppingCart';
 
 function App() {
   return (
     <>
-      <Exam2 />
+      <ShoppingCart />
     </>
   );
 }
