eb7b6b65e0edce75fc6dccf4fbcd0d06061f8f5f
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-13 15:01:22 +0800

    貨幣轉換-> 下拉選單(美金/ 日圓)

--- src/0113/2.money/Exchange.js
+++ src/0113/2.money/Exchange.js
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 
 function Exchange() {
-  //保持state的資料類型， 在應用程式執行過程中一致
+  //保持state的資料類型， 在應用程式執行過程中一致，由初始值決定，此處採用數字
   const [inputTWD, setInputTWD] = useState(0);
-  const [inputUSD, setInputUSD] = useState(0);
+  const [inputMoney, setInputMoney] = useState(0);
 
+  // 貨幣下拉選單用
+  const [currency, setCurrency] = useState('美金');
+  const currencyOptions = ['美金', '日圓'];
+
+  // 美金 -> 新台幣 轉換後取小數點後2位
   const convertUSD2TWD = (money) => {
     return (money * 30.38).toFixed(2); //求小數點後兩位
   };
+  // 新台幣 -> 美金 轉換後取小數點後2位
   const convertTWD2USD = (money) => {
     return (money / 30.38).toFixed(2); //求小數點後兩位
+  };
+  // 新台幣 -> 日圓
+  const convertTWD2JPY = (money) => {
+    return (money * 4.25).toFixed(2);
+  };
+  // 日圓 -> 新台幣
+  const convertJPY2TWD = (money) => {
+    return (money / 4.25).toFixed(2);
   };
 
   return (
@@ -18,28 +32,66 @@ function Exchange() {
       <div>
         <input
           type="number"
+          // 為了一開始不要呈現0，方便輸入
           value={inputTWD === 0 ? '' : inputTWD} //預設為空值
           onChange={(e) => {
             //因e.target.value必為字串值，為保持state資料類型一致要先轉換再設定
+            // 空字串時為數字0，即`Number('') = 0`
             const newInputTWD = Number(e.target.value);
             setInputTWD(newInputTWD);
-            setInputUSD(convertTWD2USD(newInputTWD));
+
+            if (currency === '美金') {
+              setInputMoney(convertTWD2USD(newInputTWD));
+            } else {
+              setInputMoney(convertTWD2JPY(newInputTWD));
+            }
           }}
         />
         <label>新台幣</label>
       </div>
-      <br></br>
       <div>
         <input
           type="number"
-          value={inputUSD === 0 ? '' : inputUSD}
+          // 為了一開始不要呈現0，方便輸入
+          value={inputMoney === 0 ? '' : inputMoney}
           onChange={(e) => {
-            const newInputUSD = Number(e.target.value);
-            setInputUSD(newInputUSD);
-            setInputTWD(convertUSD2TWD(newInputUSD));
+            const newInputMoney = Number(e.target.value);
+            setInputMoney(newInputMoney);
+
+            if (currency === '美金') {
+              setInputTWD(convertUSD2TWD(newInputMoney));
+            } else {
+              setInputTWD(convertJPY2TWD(newInputMoney));
+            }
           }}
         />
-        <label>美金</label>
+        <select
+          value={currency}
+          onChange={(e) => {
+            const newCurrency = e.target.value;
+            setCurrency(newCurrency);
+
+            // 下拉選單改變幣種時，直接轉換目前新台幣轉入數字為該幣種值
+
+            // if (newCurrency === '美金') {
+            //   setInputMoney(convertTWD2USD(inputTWD))
+            // } else {
+            //   setInputMoney(convertTWD2JPY(inputTWD))
+            // }
+
+            // 也可以全歸零
+            setInputTWD(0);
+            setInputMoney(0);
+          }}
+        >
+          {currencyOptions.map((v, i) => {
+            return (
+              <option key={i} value={v}>
+                {v}
+              </option>
+            );
+          })}
+        </select>
       </div>
     </>
   );
