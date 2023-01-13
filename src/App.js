6cff9696344e20b719481bd3328a64f75c4e0e91
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-13 09:52:34 +0800

    可控的表單元件example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import HTML5Form from './0113/1.form/HTML5Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HTML5Form />
+    </>
   );
 }
 
