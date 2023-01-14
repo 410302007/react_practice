d2e45eafcfb3c02e02870ac9c5942005ba00fa7c
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-14 16:33:25 +0800

    提取三個處理函式

--- src/0114/4.todo/TodoApp.js
+++ src/0114/4.todo/TodoApp.js
@@ -10,6 +10,63 @@ function TodoApp() {
     { id: 2, text: '寫作業', completed: true },
   ]);
 
+  const addTodo = (text) => {
+    // id的選擇
+    // 1. 單人使用應用，可用時間日期毫秒整數值，如`Number(new Date())`或`+new Date()`
+    // 2. 用uuid或nanoid函式庫(需另外安裝npm)
+    // 3. 使用資料庫的id產生機制
+    // 4. 其它(例如數字遞增…)，如下範例
+    // 先寫出新增的物件
+    const newTodo = {
+      id: Number(new Date()),
+      text: text,
+      completed: false, //預設為未完成
+    };
+    // 共通三步驟
+    // 1. 從目前的狀態拷貝(ps.深拷貝/完全拷貝)出一個新的變數值(陣列/物件)
+    // 2. 在新的變數值(陣列/物件)上作處理
+    // 3. 設定回原本的狀態中
+    //1 //2 //3
+    setTodos([newTodo, ...todos]);
+  };
+
+  //傳入id, 然後做completed屬性的boolean值
+  const toggleCompleted = (id) => {
+    // 1. 從目前的狀態拷貝(ps.深拷貝/完全拷貝)出一個新的變數值(陣列/物件)\
+    const newTodos = todos.map((v) => {
+      return { ...v };
+    });
+
+    const index = todos.findIndex((v, i) => {
+      return v.id === id;
+    });
+    if (index !== -1) {
+      // 2. 在新的變數值(陣列/物件)上作處理
+      newTodos[index].completed = !newTodos[index].completed;
+
+      // 3. 設定回原本的狀態中
+      setTodos(newTodos);
+    }
+    // 下面是同樣的結果的寫法
+    // const newTodos = todos.map((v2, i2) => {
+    //   if (id === v2.id)
+    //     return { ...v2, completed: !v2.completed }
+    //   else
+    //     return { ...v2 }
+    // })
+    // setTodos(newTodos)
+  };
+  // 傳入id，刪除此項目
+  const deleteTodo = (id) => {
+    //1. 2
+    const newTodos = todos.filter((v, i) => {
+      return v.id !== id;
+    });
+
+    //3
+    setTodos(newTodos);
+  };
+
   return (
     <>
       <h1>待辦事項</h1>
@@ -21,31 +78,9 @@ function TodoApp() {
         }}
         onKeyDown={(e) => {
           if (e.key === 'Enter' && e.target.value.trim() !== '') {
-            //參考0110/5.p/Exam2
-            // id
-            // 1. 單人使用應用，可用時間日期毫秒整數值，如`Number(new Date())`或`+new Date()`
-            // 2. 用uuid或nanoid函式庫(需另外安裝npm)
-            // 3. 使用資料庫的id產生機制
-            // 4. 其它(例如數字遞增…)，如下範例
-            // const ids = data.map((v) => v.id)
-            // const maxId = Math.max(...ids)
-            // const newId = maxId + 1
+            //新增todo
+            addTodo(e.target.value);
 
-            // 先寫出新增的物件
-            const newTodo = {
-              id: Number(new Date()),
-              text: e.target.value,
-              completed: false, //預設為未完成
-            };
-            // 共通三步驟
-            // 1. 從目前的狀態拷貝(ps.深拷貝/完全拷貝)出一個新的變數值(陣列/物件)
-            // 2. 在新的變數值(陣列/物件)上作處理
-            // 3. 設定回原本的狀態中
-
-            //1 & 2
-            const newTodos = [newTodo, ...todos];
-            //3.
-            setTodos(newTodos);
             //清空文字輸入框
             setInputText('');
           }
@@ -60,31 +95,18 @@ function TodoApp() {
               <input
                 type="checkbox"
                 checked={v.completed}
-                onChange={(e) => {
-                  //1.從目前的狀態拷貝(ps.深拷貝/完全拷貝)出一個新的變數值(陣列/物件)\
-                  const newTodos = todos.map((v2) => {
-                    return { ...v2 };
-                  });
-                  // 其它深拷貝語法
-                  //const newTodos = JSON.parse(JSON.stringify(todos))
-
-                  //2.在新的變數值(陣列/物件)上做處理
-                  newTodos[i].completed = !newTodos[i].completed;
-
-                  //3.設定回原本的狀態中
-                  setTodos(newTodos);
-
-                  // 下面是同樣的結果的寫法
-                  // const newTodos = todos.map((v2, i2) => {
-                  //   if (v.id === v2.id)
-                  //     return { ...v2, completed: !v2.completed }
-                  //   else
-                  //     return { ...v2 }
-                  // })
-                  //setTodos(newTodos)
+                onChange={() => {
+                  toggleCompleted(v.id);
                 }}
               />
               {v.text}
+              <button
+                onClick={(e) => {
+                  deleteTodo(v.id);
+                }}
+              >
+                刪除
+              </button>
             </li>
           );
         })}
