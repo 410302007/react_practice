992c4342ea44e6d26fe9c565c6ee868cfb363fa1
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-17 00:21:12 +0800

    props- child to child

--- src/0110/3-2.props-2/Parent.js
+++ src/0110/3-2.props-2/Parent.js
@@ -3,14 +3,14 @@ import ChildA from './ChildA';
 import ChildB from './ChildB';
 
 function Parent() {
-  // 父母中的內部資料
-  const [pData, setPData] = useState('parent data');
+  //父母內部無資料，子女b將自己內部資料給父母，父母再傳給子女A
+
   //準備接收來自子女B的資料
   const [dataFromChild, setDataFromChild] = useState('');
   return (
     <>
       <h1>Parent</h1>
-
+      {/* 給子女A可以設定state的props */}
       <ChildA dataFromChild={dataFromChild} />
       {/* 給子女B可以設定state的函式 */}
       <ChildB setDataFromChild={setDataFromChild} />
