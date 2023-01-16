a08f88d03b787a01a4159af1d28c97fb9a188091
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-16 22:10:02 +0800

    1.state異步用useEffect解決

--- src/0109/7.state/counter.js
+++ src/0109/7.state/counter.js
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Counter() {
   const [total, setTotal] = useState(0);
+
+  // const [totalTwo, setTotalTwo] = useState(total + 1); //只能作為初始化用，一開始只會執行一次，之後並不會再呼叫
+
+  const [totalTwo, setTotalTwo] = useState(0);
+  const [totalThree, setTotalThree] = useState(0);
 
   //異步(非同步)
   // console.log('a');
@@ -10,9 +15,18 @@ function Counter() {
   // }, 5000);    //等待5秒
   // console.log('c');
 
+  //批次改變
+  //1->2->3
+  useEffect(() => {
+    setTotalTwo(total + 1);
+  }, [total]);
+  useEffect(() => {
+    setTotalThree(totalTwo + 1);
+  }, [totalTwo]);
+
   return (
     <>
-      <h1
+      {/* <h1
         onClick={() => {
           const newTotal = total + 1;
           console.log('before call setTotal total =', total);
@@ -21,7 +35,16 @@ function Counter() {
         }}
       >
         {total}
+      </h1> */}
+      <h1
+        onClick={() => {
+          setTotal(total + 1);
+        }}
+      >
+        {total}
       </h1>
+      <h1>{totalTwo}</h1>
+      <h1>{totalThree}</h1>
     </>
   );
 }
