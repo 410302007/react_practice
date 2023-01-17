be8414d6f9f4c76f588f67c59dc4fcd0a6981be9
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-17 16:35:40 +0800

    useEffect-完全磨米didUpdate

--- src/0117/2.lifecycle/FC.js
+++ src/0117/2.lifecycle/FC.js
@@ -1,7 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function FC() {
   const [total, setTotal] = useState(0);
+
+  //記憶進入didMount 時間點的變數
+  //`{current:false}`
+  const didMountRef = useRef(false);
 
   //模擬didMount
   useEffect(() => {
@@ -17,6 +21,23 @@ function FC() {
   // ^^^^^^^ 相依性陣列中，用於放入與"更新階段"有關的props(從父母元件來的)或state(自己本身的)
   //意義是當放入的state或props更動時，會執行第一參數函式中的程式碼(行為模式相似於onChange)
   //只有當從父母接收到新的props或本身的state有更動時，才會觸發更新(updating/ re-render)階段
+
+  //只模擬didUpdate
+  useEffect(() => {
+    //略過初始值
+    if (total !== 0) {
+      console.log('模擬didUpdate-2', 'total=', total);
+    }
+  }, [total]);
+  //完全模擬didUpdate
+  useEffect(() => {
+    if (didMountRef.current) {
+      console.log('模擬didUpdate-3', 'total=', total);
+    } else {
+      didMountRef.current = true;
+    }
+  }, [total]);
+
   return (
     <>
       <h1>函式型元件</h1>
