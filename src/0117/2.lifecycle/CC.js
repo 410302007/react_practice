191c8d3f55f58380b9b53afb375569c6c4413719
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-17 15:59:01 +0800

    CC元件- 三個生命週期方法

--- src/0117/2.lifecycle/CC.js
+++ src/0117/2.lifecycle/CC.js
@@ -8,8 +8,24 @@ class CC extends React.Component {
     this.state = {
       total: 0,
     };
+    console.log('constructor'); //1,先執行
   }
+  //先做第一次呈現(render)，才會執行(componentDidMount)
+  componentDidMount() {
+    console.log('componentDidMount'); //3.最後執行
+  }
+  //先做第一次呈現(render)，元件已經更新完成，才會執行
+  componentDidUpdate() {
+    console.log('componentDidUpdate', 'total=', this.state.total);
+  }
+  //從dom結構裡元件完全被移除，才會呼叫
+  //元件無法移除他自己，只有父母元件才可移除
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
   render() {
+    console.log('render'); //2.執行
     return (
       <>
         <h1
