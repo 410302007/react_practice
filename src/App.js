8132978107481b3f3edbfc84099e47091ae73f5c
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-18 15:52:38 +0800

    router- link元件與a連結比較

--- src/App.js
+++ src/App.js
@@ -12,15 +12,19 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
+      <h2>a連結</h2>
+      <a href="/">home</a> <a href="member">member</a>
+      <hr />
+      <h2>Link元件</h2>
+      <Link to="/">home</Link> <Link to="member">member</Link>
+      <hr />
       <Routes>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="product" element={<Product />} />
-          <Route path="member" element={<Member />} />
-          {/*404找不到網頁用的頁面 */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="product" element={<Product />} />
+        <Route path="member" element={<Member />} />
+        {/*404找不到網頁用的頁面 */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
