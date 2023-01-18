c3b4266c3eab6cbe927fb5de75c43e39f9111ec1
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-18 15:24:45 +0800

    router-基本架構

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 // import Main from './0118/1.lifecycle/Main';
-import BS5 from './0118/3.BS5/BS5';
+// import ReactBS5 from './0118/4.react-bs/ReactBS5';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+//引入頁面元件
+import Home from './pages/Home';
+import Member from './pages/Member';
+import About from './pages/About';
+import Product from './pages/Product';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <>
-      <BS5 />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="product" element={<Product />} />
+          <Route path="member" element={<Member />} />
+          {/*404找不到網頁用的頁面 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
