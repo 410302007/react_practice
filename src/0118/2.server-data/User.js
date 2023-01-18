ea6ea48258b98c7ab6adce1d41fc1e8ac85ee740
Chu-Ya Hsu <t199511137@gmail.com>
2023-01-18 11:39:08 +0800

    載入指示器

--- src/0118/2.server-data/User.js
+++ src/0118/2.server-data/User.js
@@ -10,6 +10,9 @@ function User() {
   // }]
   const [users, setUsers] = useState([]);
 
+  //載入指示器用
+  const [isLoading, setIsLoading] = useState(true); //一開始就載入
+
   //與伺服器相連獲得資料的函式
   const getUsers = async () => {
     try {
@@ -45,31 +48,48 @@ function User() {
   useEffect(() => {
     //向伺服器要求資料
     getUsers();
+
+    //設定兩秒後關閉載入指示動畫
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
   }, []);
+
+  const loader = (
+    <div className="d-flex justify-content-center">
+      <div className="spinner-border text-danger" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+
+  const display = (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>姓名</th>
+          <th>年齡</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((v, i) => {
+          return (
+            <tr key={v.id}>
+              <td>{v.id}</td>
+              <td>{v.name}</td>
+              <td>{v.age}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 
   return (
     <>
       <h1>會員資料</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>姓名</th>
-            <th>年齡</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((v, i) => {
-            return (
-              <tr>
-                <td>{v.id}</td>
-                <td>{v.name}</td>
-                <td>{v.age}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {isLoading ? loader : display}
     </>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Main from './0118/1.lifecycle/Main';
-// import User from './0118/2.server-data/User';
+// import Main from './0118/1.lifecycle/Main';
+import User from './0118/2.server-data/User';
 
 function App() {
   return (
     <>
-      <Main />
+      <User />
     </>
   );
 }
